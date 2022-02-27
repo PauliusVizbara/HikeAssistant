4d52eebc6cdfe7d04ab337d8732dc6f976b95974
Paulius <paulius1207@gmail.com>
2022-02-27 18:09:50 +0200

    chore: remove unused fragment tag

--- src/views/WelcomeMessage/WelcomeMessage.tsx
+++ src/views/WelcomeMessage/WelcomeMessage.tsx
@@ -12,34 +12,31 @@ type WelcomeMessageProps = {
 export const WelcomeMessage: React.FC<WelcomeMessageProps> = (props) => {
   const { equipmentList } = props
   return (
-    <>
-      <SplitCard>
-        <SplitCardContent>
-          <Typography variant="h5">Welcome to Hike Assistant!</Typography>
-          <Typography variant="subtitle1">
-            Let&apos;s begin your preparations
-          </Typography>
-          <Typography sx={{ pt: 2 }} variant="body1">
-            This app will guide You through packing for the hike by providing
-            the needed essentials, as well as asking variuos questions about
-            Your journey to cater for additional needs.
-          </Typography>
-        </SplitCardContent>
-        <Divider orientation="vertical" flexItem></Divider>
-        <SplitCardContent>
-          <Typography variant="h5">Starting equipment</Typography>
-          <Divider sx={{ my: 1 }} />
-          {equipmentList
-            .filter(
-              (equipment) =>
-                equipment.checklistLocation ===
-                CHECKLIST_LOCATION.WELCOME_MESSAGE
-            )
-            .map((equipment) => (
-              <Equipment key={equipment.id} equipment={equipment} />
-            ))}
-        </SplitCardContent>
-      </SplitCard>
-    </>
+    <SplitCard>
+      <SplitCardContent>
+        <Typography variant="h5">Welcome to Hike Assistant!</Typography>
+        <Typography variant="subtitle1">
+          Let&apos;s begin your preparations
+        </Typography>
+        <Typography sx={{ pt: 2 }} variant="body1">
+          This app will guide You through packing for the hike by providing the
+          needed essentials, as well as asking variuos questions about Your
+          journey to cater for additional needs.
+        </Typography>
+      </SplitCardContent>
+      <Divider orientation="vertical" flexItem></Divider>
+      <SplitCardContent>
+        <Typography variant="h5">Starting equipment</Typography>
+        <Divider sx={{ my: 1 }} />
+        {equipmentList
+          .filter(
+            (equipment) =>
+              equipment.checklistLocation === CHECKLIST_LOCATION.WELCOME_MESSAGE
+          )
+          .map((equipment) => (
+            <Equipment key={equipment.id} equipment={equipment} />
+          ))}
+      </SplitCardContent>
+    </SplitCard>
   )
 }
